8add80b3c478381f911b3bd89ad7d2bcec24e0ce
Jaydeep Dey <75583927+jaydeepdey03@users.noreply.github.com>
2023-05-19 00:42:48 +0530

    Update Toggletheme.jsx

--- src/components/Toggletheme.jsx
+++ src/components/Toggletheme.jsx
@@ -1,6 +1,5 @@
 import { Button, useColorMode } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
-import React from 'react'
 
 const ToggleTheme = () => {
     const { colorMode, toggleColorMode } = useColorMode()
